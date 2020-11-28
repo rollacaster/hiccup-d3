9aa8f77bc988c7a7b586ff972e9170ce673a105e
Thomas Sojka <thsojka@web.de>
2020-11-28 11:21:21 +0100

    Refactor: Create container for charts

--- src/main/tech/thomas_sojka/hiccup_d3/app.cljs
+++ src/main/tech/thomas_sojka/hiccup_d3/app.cljs
@@ -42,9 +42,9 @@
           margin {:top 0 :right 0 :left 13 :bottom 0}
           x (-> (d3/scaleLinear)
                 (.range (clj->js [(:left margin) (- size (:right margin))]))
-                (.domain (clj->js [0 (apply max (map :frequency @data))])))
+                (.domain (clj->js [0 (apply max (map :frequency data))])))
           y (-> (d3/scaleBand)
-                (.domain (clj->js (range (count @data))))
+                (.domain (clj->js (range (count data))))
                 (.range (clj->js [(:top margin) (- size (:bottom margin))])))
           color (d3/scaleOrdinal d3/schemeCategory10)]
       [:svg {:viewBox (str "0 0 " size " " size)}
@@ -56,7 +56,7 @@
                    :fill (color letter)
                    :width (x frequency)}]
            ])
-        @data)
+        data)
        (map-indexed
         (fn [idx {:keys [letter frequency]}]
           [:g {:key idx :transform (str "translate("0 "," (y idx) ")")}
@@ -67,26 +67,29 @@
            [:text.current-fill
             {:x 0 :y (/ (.bandwidth y) 2) :dominant-baseline "middle" :style {:font-size 10}}
             (str letter)]])
-        @data)])})
+        data)])})
+
+(defn chart-container [{:keys [title chart code data]}]
+  [:div {:class "w-1/2"}
+   [:h3.text-lg.font-bold.mb-2 title]
+   [:div.mb-2 [chart @data]]
+   [:pre.overflow-scroll.mb-2
+    (with-out-str (pprint code))]
+   [:pre.overflow-scroll
+    (with-out-str (pprint @data))]])
 
 (defn app []
   (-> (js/fetch "data/frequencies.json")
-        (.then (fn [res] (.json res)))
-        (.then (fn [res] (reset! (:data bar) (js->clj res :keywordize-keys true))))
-        (.catch (fn [res] (prn res))))
+      (.then (fn [res] (.json res)))
+      (.then (fn [res] (reset! (:data bar) (js->clj res :keywordize-keys true))))
+      (.catch (fn [res] (prn res))))
   (fn []
     [:div
      [:header.border-b
       [:div.px-6.py-4.max-w-7xl.mx-auto
        [:h1.text-xl.font-bold "hiccup-d3"]]]
      [:div.max-w-7xl.mx-auto.p-6
-      [:div {:class "w-1/2"}
-       [:h3.text-lg.font-bold.mb-2 "Bar Chart"]
-       [:div.mb-2 [(:chart bar) @(:data bar)]]
-       [:pre.overflow-scroll.mb-2
-        (with-out-str (pprint (:code bar)))]
-       [:pre.overflow-scroll
-        (with-out-str (pprint @(:data bar)))]]]]))
+      [chart-container bar]]]))
 
 (dom/render [app] (js/document.getElementById "root"))
 
