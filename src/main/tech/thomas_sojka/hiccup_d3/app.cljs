a8dadcbc41e974060c25eb1f5b8e527160266030
Thomas Sojka <thsojka@web.de>
2020-11-28 12:55:18 +0100

    Adjust bar chart code

--- src/main/tech/thomas_sojka/hiccup_d3/app.cljs
+++ src/main/tech/thomas_sojka/hiccup_d3/app.cljs
@@ -39,36 +39,37 @@
                     (str letter)]])
                 data)]))
    :code
-   '(let [size 300
-          margin {:top 0 :right 0 :left 13 :bottom 0}
-          x (-> (d3/scaleLinear)
-                (.range (clj->js [(:left margin) (- size (:right margin))]))
-                (.domain (clj->js [0 (apply max (map :frequency data))])))
-          y (-> (d3/scaleBand)
-                (.domain (clj->js (range (count data))))
-                (.range (clj->js [(:top margin) (- size (:bottom margin))])))
-          color (d3/scaleOrdinal d3/schemeCategory10)]
-      [:svg {:viewBox (str "0 0 " size " " size)}
-       (map-indexed
-        (fn [idx {:keys [letter frequency]}]
-          [:g {:key idx :transform (str "translate("0 "," (y idx) ")")}
-           [:rect {:x (x 0)
-                   :height (.bandwidth y)
-                   :fill (color letter)
-                   :width (x frequency)}]
-           ])
-        data)
-       (map-indexed
-        (fn [idx {:keys [letter frequency]}]
-          [:g {:key idx :transform (str "translate("0 "," (y idx) ")")}
-           [:text {:style {:font-size 8}
-                   :x 17
-                   :y (/ (.bandwidth y) 2)
-                   :dominant-baseline "middle"} (str frequency)]
-           [:text.current-fill
-            {:x 0 :y (/ (.bandwidth y) 2) :dominant-baseline "middle" :style {:font-size 10}}
-            (str letter)]])
-        data)])})
+   '(fn [data]
+      (let [size 300
+            margin {:top 0 :right 0 :left 13 :bottom 0}
+            x (-> (d3/scaleLinear)
+                  (.range (clj->js [(:left margin) (- size (:right margin))]))
+                  (.domain (clj->js [0 (apply max (map :frequency data))])))
+            y (-> (d3/scaleBand)
+                  (.domain (clj->js (range (count data))))
+                  (.range (clj->js [(:top margin) (- size (:bottom margin))])))
+            color (d3/scaleOrdinal d3/schemeCategory10)]
+        [:svg {:viewBox (str "0 0 " size " " size)}
+         (map-indexed
+          (fn [idx {:keys [letter frequency]}]
+            [:g {:key idx :transform (str "translate("0 "," (y idx) ")")}
+             [:rect {:x (x 0)
+                     :height (.bandwidth y)
+                     :fill (color letter)
+                     :width (x frequency)}]
+             ])
+          data)
+         (map-indexed
+          (fn [idx {:keys [letter frequency]}]
+            [:g {:key idx :transform (str "translate("0 "," (y idx) ")")}
+             [:text {:style {:font-size 8}
+                     :x 17
+                     :y (/ (.bandwidth y) 2)
+                     :dominant-baseline "middle"} (str frequency)]
+             [:text.current-fill
+              {:x 0 :y (/ (.bandwidth y) 2) :dominant-baseline "middle" :style {:font-size 10}}
+              (str letter)]])
+          data)]))})
 
 (def code (:code bar))
 (defn chart-container []
