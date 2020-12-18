dd17eabf210bea03d5f0997e6001cec10209d18b
Thomas Sojka <thsojka@web.de>
2020-12-18 06:58:53 +0100

    Refactor chord

--- src/main/tech/thomas_sojka/hiccup_d3/app.cljs
+++ src/main/tech/thomas_sojka/hiccup_d3/app.cljs
@@ -330,26 +330,27 @@
     (fn [data]
       (let [size 300
             radius (/ size 2)
+            innerRadius (- radius 10)
             color (d3/scaleOrdinal d3/schemeCategory10)
             ribbon (-> (d3/ribbon)
-                       (.radius (- radius 1)))
+                       (.radius (- innerRadius 1))
+                       (.padAngle (/ 1 innerRadius)))
             arc (-> (d3/arc)
-                    (.innerRadius (- radius 10))
+                    (.innerRadius innerRadius)
                     (.outerRadius radius))
             chord (-> (d3/chord)
                       (.sortSubgroups d3/descending)
                       (.sortChords d3/descending))]
-
         [:svg {:viewBox (str (- radius) " " (- radius) " " size " " size)}
          [:g
           (map-indexed
-           (fn [idx group]
-             [:path {:key idx :d (ribbon group) :fill (color idx)}])
+           (fn [idx link]
+             [:path {:key idx :d (ribbon link) :fill (color ^js (.-source.index link))}])
            (chord data))]
          [:g
-          (map-indexed
-           (fn [idx group]
-             [:path {:d (arc group) :fill (color idx) :key idx}])
+          (map
+           (fn [group]
+             [:path {:d (arc group) :fill (color (.-index group)) :key (.-index group)}])
            (.-groups (chord data)))]]))}))
 
 (def contour
